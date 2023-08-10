3402bc15f6e545395879f9fa6dcfc38b4deb932a
alishamseer7 <alishamseer@gmail.com>
2023-08-10 10:23:30 +0530

    made input responsive

--- script.js
+++ script.js
@@ -2,11 +2,24 @@
 
 const apiUrl = 'https://newton.now.sh/api/v2';
 
+async function getAnswer (op, exp) {
+    try {
+        const response = await fetch(`${apiUrl}/${op}/${exp}`);
+        const result = await response.json();
+        console.log(result.result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getAnswer('abs', -5);
+
 const searchBtn = document.getElementById('search-btn');
-searchBtn.addEventListener('click', function () {
+searchBtn.addEventListener('click', function (e) {
     const problemInput = document.getElementById('problem');
     const operationCategory = document.getElementById('category');
     const operation = operationCategory.value;
+    const capitalizedOperation = operation.charAt(0).toUpperCase() + operation.slice(1);
     const expression = problemInput.value;
 
     if (expression === '') {
@@ -18,7 +31,7 @@ searchBtn.addEventListener('click', function () {
         answerContainer.classList.add('answer-container');
         resultContainer.appendChild(answerContainer);
         const h3 = document.createElement('h3');
-        h3.textContent = `${operation}: ${expression}`;
+        h3.textContent = `${capitalizedOperation}: ${expression}`;
         answerContainer.appendChild(h3);
     }
 })
