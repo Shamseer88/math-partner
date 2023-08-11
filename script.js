6b2151d911724ad81dc2eafc70c00606cc1f3483
alishamseer7 <alishamseer@gmail.com>
2023-08-11 12:12:50 +0530

    created result container using js

--- script.js
+++ script.js
@@ -6,32 +6,37 @@ async function getAnswer (op, exp) {
     try {
         const response = await fetch(`${apiUrl}/${op}/${exp}`);
         const result = await response.json();
-        console.log(result.result);
+        // console.log (result.result);
+        return result.result
     } catch (error) {
         console.log(error);
     }
 }
 
-getAnswer('abs', -5);
 
 const searchBtn = document.getElementById('search-btn');
-searchBtn.addEventListener('click', function (e) {
+const resultContainer = document.querySelector('.result');
+const answerContainer = document.createElement('div');
+answerContainer.classList.add('answer-container');
+
+searchBtn.addEventListener('click', async function (e) {
     const problemInput = document.getElementById('problem');
     const operationCategory = document.getElementById('category');
     const operation = operationCategory.value;
     const capitalizedOperation = operation.charAt(0).toUpperCase() + operation.slice(1);
     const expression = problemInput.value;
+    resultContainer.appendChild(answerContainer);
 
     if (expression === '') {
         alert("You have to enter a problem!");
     }
     else {
-        const resultContainer = document.querySelector('.result');
-        const answerContainer = document.createElement('div');
-        answerContainer.classList.add('answer-container');
-        resultContainer.appendChild(answerContainer);
-        const h3 = document.createElement('h3');
-        h3.textContent = `${capitalizedOperation}: ${expression}`;
-        answerContainer.appendChild(h3);
+        const finalResult = await getAnswer(operation, expression);
+        console.log(finalResult)
+        answerContainer.innerHTML = `
+            <h3>${capitalizedOperation}: ${expression}</h3>
+            <p>${finalResult}</p>
+            <i class="fa-solid fa-trash-can"></i>
+        `       
     }
 })
