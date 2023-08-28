d0ec5e8ba83e60b04214b04195d64fe0e290c02e
Shamseer88 <alishamseer@gmail.com>
2023-08-28 11:57:10 +0530

    local storage addition

--- script.js
+++ script.js
@@ -1,42 +1,88 @@
 // https://github.com/aunyks/newton-api
 
-const apiUrl = 'https://newton.now.sh/api/v2';
+const apiUrl = "https://newton.now.sh/api/v2";
 
-async function getAnswer (op, exp) {
-    try {
-        const response = await fetch(`${apiUrl}/${op}/${exp}`);
-        const result = await response.json();
-        // console.log (result.result);
-        return result.result
-    } catch (error) {
-        console.log(error);
-    }
+let solvedProblemsArray = getSolvedProblems();
+
+function addSolvedProblems(operation, expression, result) {
+  solvedProblemsArray = [
+    ...solvedProblemsArray,
+    { operation, expression, result },
+  ];
+  addSolvedProblemsToLocalStorage(solvedProblemsArray);
 }
 
+function addSolvedProblemsToLocalStorage(solvedProblems) {
+  const solvedProblemsJSON = JSON.stringify(solvedProblems);
+  localStorage.setItem("solvedProblems", solvedProblemsJSON);
+}
 
-const searchBtn = document.getElementById('search-btn');
-const resultContainer = document.querySelector('.result');
-const answerContainer = document.createElement('div');
-answerContainer.classList.add('answer-container');
+function getSolvedProblems() {
+  const retrievedSolvedProblemsJSON = localStorage.getItem("solvedProblems");
+  return retrievedSolvedProblemsJSON
+    ? JSON.parse(retrievedSolvedProblemsJSON)
+    : [];
+}
 
-searchBtn.addEventListener('click', async function (e) {
-    const problemInput = document.getElementById('problem');
-    const operationCategory = document.getElementById('category');
-    const operation = operationCategory.value;
-    const capitalizedOperation = operation.charAt(0).toUpperCase() + operation.slice(1);
-    const expression = problemInput.value;
-    resultContainer.appendChild(answerContainer);
+async function getAnswer(op, exp) {
+  try {
+    const response = await fetch(`${apiUrl}/${op}/${exp}`);
+    const result = await response.json();
+    // console.log (result.result);
+    // console.log(result);
+    addSolvedProblems(result.operation, result.expression, result.result);
+    return result.result;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-    if (expression === '') {
-        alert("You have to enter a problem!");
+const searchBtn = document.getElementById("search-btn");
+const resultContainer = document.querySelector(".result");
+const answerContainer = document.createElement("div");
+answerContainer.classList.add("answer-container");
+
+searchBtn.addEventListener("click", async function (e) {
+  const problemInput = document.getElementById("problem");
+  const operationCategory = document.getElementById("category");
+  const operation = operationCategory.value;
+  const capitalizedOperation =
+    operation.charAt(0).toUpperCase() + operation.slice(1);
+  const expression = problemInput.value;
+  resultContainer.appendChild(answerContainer);
+
+  if (expression === "") {
+    alert("You have to enter a problem!");
+  } else {
+    try {
+      const finalResult = await getAnswer(operation, expression);
+      console.log(finalResult);
+      answerContainer.innerHTML = `
+                <h3>${capitalizedOperation}: ${expression}</h3>
+                <p>${finalResult}</p>
+                <i class="fa-solid fa-trash-can"></i>
+            `;
+    } catch (error) {
+      console.log(error);
     }
-    else {
-        const finalResult = await getAnswer(operation, expression);
-        console.log(finalResult)
-        answerContainer.innerHTML = `
-            <h3>${capitalizedOperation}: ${expression}</h3>
-            <p>${finalResult}</p>
-            <i class="fa-solid fa-trash-can"></i>
-        `       
-    }
-})
+    problemInput.value = "";
+  }
+});
+
+// Getting result history
+console.log(solvedProblemsArray[0].operation);
+const solvedSolutionsBtn = document.querySelector(".solved-solutions-btn");
+const tableBody = document.getElementById("table-body");
+
+solvedSolutionsBtn.addEventListener("click", function () {
+  solvedProblemsArray.forEach((solution) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+                    <td>${solution.expression}</td>
+                    <td>${solution.operation}</td>
+                    <td>${solution.result}</td>
+                    <td class="close-btn-td"><i class="fa-regular fa-circle-xmark"></i></td>
+                    `;
+    tableBody.appendChild(tr);
+  });
+});
